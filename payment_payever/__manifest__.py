0d00fd5533f879769a3ea970c97f38fb7e32d331
payeverdev <service@payever.de>
2026-04-02 14:12:12 +0800

    Update __manifest__.py

--- payment_payever/__manifest__.py
+++ payment_payever/__manifest__.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 {
-    'name': 'payever Payments',
-    'version': '19.0.1.0.0',
+    'name': 'payever Checkout',
+    'version': '1.0.0',
     'category': 'eCommerce',
     'license': 'LGPL-3',
     'author': 'payever GmbH',
